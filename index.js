7a788bcd76251e11576ee5533fe34e937d504c38
Indira Kissoondyal <indirak@alumni.princeton.edu>
2019-01-27 02:11:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function ternaryCheckCity(city){
 function switchOnCharmFromTip(tip) {
   if (tip === "generous") {
     return 'Thank you so much.'}
-    else if (tip === "not generous") {
+    else if (tip === "not as generous") {
       return 'Thank you.'}
       else {
         return "Bye."}
